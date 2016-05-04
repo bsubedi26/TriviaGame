72821543608dabe5138d16a20312488d2490b25c
Bibek Subedi <bsubedi26@gmail.com>
2016-05-03 23:15:30 -0400

    Added more javascript and better font styling with few css changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-var allow = false; //Global boolean to start/stop the countdown timer
+
+var allow = false; //Global boolean
+
 function main() {
 	//Declare variables
 	var correct = 0;
@@ -7,8 +9,8 @@ function main() {
 	var unanswered = 0;
 	var questions = [
 	{
-	question : "What is sushi traditionally wrapped in?",
-	choices: ["Edible SeaWeed", "Tobacco Leaf", "Rolling Papers"],
+	question : "Which of the following is NOT a valid JSON object?",
+	choices: ["seaWeed", "Tobacco Leaf", "Rolling Papers"],
 	correctAnswer: 0
 	},
 	{
@@ -20,13 +22,24 @@ function main() {
 	question : "If you had Lafite-Rothchild on your dinner table, what would it be?",
 	choices: ["Alcohol", "Wine", "Sauce"],
 	correctAnswer: 1
+	},
+	{
+	question : "Who is the current president of the United States",
+	choices: ["Nancy Peloski", "Tony Blair", "Barack Obama"],
+	correctAnswer: 1
+	},
+	{
+	question : "Who is the current attorney general of the federal government?",
+	choices: ["Harry S. Truman", "Judge Judy", "Eric Holder"],
+	correctAnswer: 1
 	}
 ]
+
 //function to get a random object property
 function randomProperty(obj) {
     var keys = Object.keys(obj)
     return obj[keys[Math.floor(Math.random() * keys.length)]];
-};
+}
 
 var randomQuestion = randomProperty(questions);
 console.log(randomQuestion)
@@ -51,8 +64,83 @@ $("#choices").append('<button>'+randomQuestion.choices[i]+'</button>');
 				correct++;
 				console.log("right");
 				allow = true;
+				$("#question").empty();
+				$("#choices").html("You are correct!")
+				setTimeout(reset, 2000);
+				
+			}
+			else {
+				incorrect++;
+				console.log("wrong");
+				allow = true;
+				$("#question").empty();
+				$("#choices").html("You are wrrrrrrooooonnngggggg! The correct answer: " + randomQuestion.choices[randomQuestion.correctAnswer])
+				setTimeout(reset, 2000);
+			}
+
+		})
+
+// Countdown Timer
+var count=30;
+var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+function timer() {
+  count=count-1;
+$("#time").html("Time Left: " + count);
+  if (count <= 0)
+  {
+     //counter ended, do something here
+     clearInterval(counter);
+
+     
+  }
+  if (allow == true) {
+  	return false;
+  }
+  }
+timer();
+
+
+function reset() {
+$("#time").empty();
+$("#question").empty();
+$("#choices").empty();
+// Countdown Reset
+count = 30;
+
+console.log(questions)
+console.log(oldQuestions)
+
+//Remove the question that was displayed in order to avoid any duplicate questions
+for (var i = 0; i < oldQuestions.length; i++) {
+	for (var j = 0; j < questions.length; j++) {
+	if (oldQuestions[i] == questions[j]) {
+		delete questions[j];
+
+		
+	}
+  }
+}
+randomQuestion = randomProperty(questions);
+oldQuestions.push(randomQuestion);
+
+//Append the random question
+$("#question").append(randomQuestion.question +"<br>");
+var multipleChoice = randomQuestion.choices;
+
+//Loop thru the choices array and add it to a list to display it better
+for (var i = 0; i < randomQuestion.choices.length; i++) {
+$("#choices").append('<button>'+randomQuestion.choices[i]+'</button>');
+}
+
+	$("#choices button").on("click", function() {
+			//Gets user answer guess
+			var userAnswer = $(this).text();
+			if (multipleChoice.indexOf(userAnswer) == randomQuestion.correctAnswer) {
+				correct++;
+				console.log("right");
+				allow = true;
 				reset();
-				//Go to the next question (run a function?)
+				
 			}
 			else {
 				incorrect++;
@@ -63,59 +151,16 @@ $("#choices").append('<button>'+randomQuestion.choices[i]+'</button>');
 
 		})
 
-// Countdown Timer
-
-var count=30;
-var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
-function timer() {
-  count=count-1;
-$("#time").html("Time Left: " + count);
-  if (count <= 0)
-  {
-     clearInterval(counter);
-     //counter ended, do something here
-     return false;
-  }
-  if (allow == true) {
-  	return false;
-  }
-  
-  //Shows the number of seconds here
-}
-
-timer();
-
-function reset() {
-$("#time").empty();
-$("#question").empty();
-$("#choices").empty();
-// Countdown Reset
-count = 30;
-
-
-//Append the random question
-if (!(questions in oldQuestions)) {
-	console.log("no duplicate questions")
-randomQuestion = randomProperty(questions);
-}
-$("#question").append(randomQuestion.question +"<br>");
-var multipleChoice = randomQuestion.choices;
-
-//Loop thru the choices array and add it to a list to display it better
-for (var i = 0; i < randomQuestion.choices.length; i++) {
-$("#choices").append('<button>'+randomQuestion.choices[i]+'</button>');
-}
-oldQuestions.push(randomQuestion);
-console.log(oldQuestions)
-
 }
 
 //main closer
 }
 
+
 $(".btn").on("click", function() {
 	//Remove start button
 	$('#button').remove();
+	$("p").empty();
 	main();
 	
 
